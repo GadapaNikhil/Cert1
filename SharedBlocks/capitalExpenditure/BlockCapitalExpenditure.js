aa8867c670e688d4c7cc564dc028fb5d79a3df79
GadapaNikhil <92427003+GadapaNikhil@users.noreply.github.com>
2021-10-18 17:13:14 +0530

    First Commit
    My First Commit

---
+++ SharedBlocks/capitalExpenditure/BlockCapitalExpenditure.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
+	"use strict";
+
+	var BlockMailing = BlockBase.extend("sap.uxap.sample.SharedBlocks.capitalExpenditure.BlockCapitalExpenditure", {
+		metadata: {}
+	});
+
+	return BlockMailing;
+});
