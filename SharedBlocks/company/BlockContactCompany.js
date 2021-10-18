acb7c437a662692905c82f667c5c28747eec5bbb
chowkhan-firoz-khan <firoz90.sap@gmail.com>
2021-10-18 17:46:44 +0530

    Coming the revert

--- SharedBlocks/company/BlockContactCompany.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
-	"use strict";
-
-	var BlockMailing = BlockBase.extend("sap.uxap.sample.SharedBlocks.company.BlockContactCompany", {
-		metadata: {}
-	});
-
-	return BlockMailing;
-});
--- SharedBlocks/company/BlockContactPerson.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define(["sap/uxap/BlockBase"], function (BlockBase) {
-	"use strict";
-
-	var BlockAdresses = BlockBase.extend("sap.uxap.sample.SharedBlocks.company.BlockContactPerson", {
-		metadata: {}
-	});
-	return BlockAdresses;
-});
