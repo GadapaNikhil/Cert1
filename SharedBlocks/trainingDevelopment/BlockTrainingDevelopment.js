62c409ef50b9a597e642af92ca10b6c91d010d68
chowkhan-firoz-khan <firoz90.sap@gmail.com>
2021-10-19 12:06:10 +0530

    check

---
+++ SharedBlocks/trainingDevelopment/BlockTrainingDevelopment.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
+	"use strict";
+
+	var BlockMailing = BlockBase.extend("sap.uxap.sample.SharedBlocks.trainingDevelopment.BlockTrainingDevelopment", {
+		metadata: {}
+	});
+
+	return BlockMailing;
+});
---
+++ controller/trainingDevelopment/trainingDevelopment.controller.js
@@ -0,0 +1,46 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.uxap.sample.ObjectPageOnJSON.controller.trainingDevelopment.TrainingDevelopment", {
+		onInit: function() {
+			var oModel = new JSONModel();
+				// this.localModel = new sap.ui.model.json.JSONModel();
+				// this.getView().setModel(this.localModel, "localModel");
+				// var items = [];
+				// this.localModel.setProperty("/items", items);
+			var oData = {
+				modelData: [
+					{productive: "", Dec18: "$0", total: "$28,07,942", mft: "YES", percent: "100%", ika: "$28,07,942"},
+					{productive: "", Dec18: "$0", total: "$0", mft: "YES", percent: "20%", ika: "$0"},
+					{productive: "", Dec18: "$0", total: "$0", mft: "YES", percent: "100%", ika: "$0"},
+					{productive: "", Dec18: "$0", total: "$0", mft: "YES", percent: "20%", ika: "$0"}
+				]
+			};
+			var oView = this.getView();
+
+			oModel.setData(oData);
+			oView.setModel(oModel);
+		},
+		onAddRowContract:function(){
+			var oAddc = {
+					"supplier": "",
+					"description": "",
+					"street": "",
+					"city": "",
+					"phone":"",
+					"openOrders":""
+				};
+
+				var oModel = this.getView().getModel("localModel");
+				var aEntries = oModel.getData().oData;
+				aEntries.push(oAddc);
+				// oModel.setData({
+				// 	ServiceRequest: aEntries
+				// });
+				oModel.setProperty("/oData", aEntries);
+		}
+	});
+});
